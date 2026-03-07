12cc38cba41aad735a7be828faff75b40bbd6f89
sonuku725020-commits <sonuku725020@gmail.com>
2026-03-07 16:34:21 +0530

    search insert postion

---
+++ Array/03_search_insert_position.py
@@ -0,0 +1,20 @@
+nums = [1,3,5,7,9,11,13,15,17,19]
+low = 0
+high = len(nums) - 1
+target = 10
+while low <= high:
+    mid = low + (high - low) // 2
+    if nums[mid] == target:
+        print(mid)
+        break
+    elif nums[mid] < target:
+        low = mid + 1
+    else:
+        high = mid - 1
+else:
+    for i in range(len(nums)):
+        if nums[i] > target:
+            print(i)
+            break
+    else:
+        print(len(nums))
