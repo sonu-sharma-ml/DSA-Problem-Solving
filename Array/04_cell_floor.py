46eceacfe4c3520046c76c035d3b778018e40dec
sonuku725020-commits <sonuku725020@gmail.com>
2026-03-09 15:44:17 +0530

    cell_floor_problem

--- Array/04_cell_floor.py
+++ Array/04_cell_floor.py
@@ -17,14 +17,44 @@
 
 num = [1,2,3,4,5,7,8,12,23]
 target = 9
+floor = -1
+cell = -1   
+low = 0
+high = len(num) - 1
+while low <= high:
+    mid = low + (high - low) // 2
 
-for i in range(len(num)):
-    if num[i] == target:
-        print(num[i], num[i])
+    if num[mid] == target:
+        print(num[mid], mid)
         break
-    else: 
-        if target > num[i] and target < num[i + 1]:
-            print(num[i] , -1)
-            break
-else:
-    print(num[-1], -1)
+    elif num[mid] < target:
+        floor = num[mid]
+        low = mid + 1
+    else:
+        cell = num[mid]
+        high = mid - 1
+print(floor, cell)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for i in range(len(num)):
+#     if num[i] == target:
+#         print(num[i], num[i])
+#         break
+#     else: 
+#         if target > num[i] and target < num[i + 1]:
+#             print(num[i] , -1)
+#             break
+# else:
+#     print(num[-1], -1)
