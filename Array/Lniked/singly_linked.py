beacd3a419e05a0c82f0c3e45b391a08e1b0e685
sonuku725020-commits <sonuku725020@gmail.com>
2026-03-20 09:18:31 +0530

    dsa question

---
+++ Array/Lniked/singly_linked.py
@@ -0,0 +1,37 @@
+class node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class linkedlist:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        else:
+            current_node = self.head
+            while current_node.next is not None:
+                current_node = current_node.next
+            current_node.next = new_node
+
+    def display(self):   # ✅ FIX: inside class
+        current_node = self.head
+        while current_node is not None:
+            print(current_node.data, end=" -> ")
+            current_node = current_node.next
+        print("None")
+
+
+# Driver code
+linked_list = linkedlist()
+linked_list.append(10)
+linked_list.append(20)
+linked_list.append(30)
+linked_list.append(40)
+
+linked_list.display()
+linked_list.display()
